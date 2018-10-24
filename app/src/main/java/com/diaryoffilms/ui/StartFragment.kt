d61b521b8af8f93c260b42d643c13f613e2e431f
Alexey Katsuro <katsuro.alexey@gmail.com>
2018-10-24 19:30:44 +0300

    create layouts

--- app/src/main/java/com/diaryoffilms/ui/StartFragment.kt
+++ app/src/main/java/com/diaryoffilms/ui/StartFragment.kt
@@ -1,20 +1,28 @@
 package com.diaryoffilms.ui
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.fragment.app.Fragment
+import com.diaryoffilms.R
 import com.diaryoffilms.databinding.FragmentStartBinding
+import com.diaryoffilms.extensions.setSupportActionBar
+import kotlinx.android.synthetic.main.include_appbar.view.*
 
-class StartFragment: Fragment() {
+class StartFragment : Fragment() {
 
     private lateinit var binding: FragmentStartBinding
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-
-        binding = FragmentStartBinding.inflate(inflater,container,false)
+        setHasOptionsMenu(true)
+        binding = FragmentStartBinding.inflate(inflater, container, false)
+        val toolbar = binding.fragmentStartCoordinatorLayout.toolbar
+        setSupportActionBar(toolbar)
 
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.main_menu, menu)
     }
 }
